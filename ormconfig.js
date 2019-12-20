9dce94763bbf26d738acf103572eda8297a7fde9
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-20 21:58:52 +0300

    Configured orm

---
+++ ormconfig.js
@@ -0,0 +1,38 @@
+const logLevel = ['error', 'warn', 'schema'];
+
+if (process.env.DEBUG === 'true' && process.env.NODE_ENV === 'development') {
+  logLevel.push('query');
+}
+
+module.exports = {
+   "type": "mysql",
+   "host": process.env.DB_HOST,
+   "port": process.env.DB_PORT,
+   "username": process.env.DB_USER,
+   "password": process.env.DB_PASS,
+   "database": process.env.DB_NAME,
+   "synchronize": true,
+   "logging": logLevel,
+   cache: {
+      type:       'redis',
+      duration:   30000,
+      options: {
+        host:     process.env.REDIS_HOST || '',
+        port:     process.env.REDIS_PORT || ''
+      }
+    },
+   "entities": [
+      "src/entity/**/*.ts"
+   ],
+   "migrations": [
+      "src/migration/**/*.ts"
+   ],
+   "subscribers": [
+      "src/subscriber/**/*.ts"
+   ],
+   "cli": {
+      "entitiesDir": "src/entity",
+      "migrationsDir": "src/migration",
+      "subscribersDir": "src/subscriber"
+   }
+}
