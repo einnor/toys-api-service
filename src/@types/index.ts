71375ce315d7aff14e851219ae384de2dc26c432
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-20 22:39:25 +0300

    Added interface for PagedResponse

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -2,3 +2,4 @@ export { SearchOptions } from './SearchOptions';
 export { SortOrder } from './SortOrder';
 export { GetRequestOptions } from './GetRequestOptions';
 export { PaginationOptions } from './PaginationOptions';
+export { PagedResponse } from './PagedResponse';
