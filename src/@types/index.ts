7ff4895f7ea6b363b2ce9d0b5842a1e4be95b5da
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-20 22:37:56 +0300

    Added interface for PaginationOptions

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,3 +1,4 @@
 export { SearchOptions } from './SearchOptions';
 export { SortOrder } from './SortOrder';
 export { GetRequestOptions } from './GetRequestOptions';
+export { PaginationOptions } from './PaginationOptions';
