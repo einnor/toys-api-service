3c95334a3321b1d80f42aba1f860015dd9134d0b
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-21 18:56:07 +0300

    Refactoring api interfaces into a dedicated directory

---
+++ src/@types/api/index.ts
@@ -0,0 +1,9 @@
+export { SearchOptions } from './SearchOptions';
+export { SortOrder } from './SortOrder';
+export { GetRequestOptions } from './GetRequestOptions';
+export { PaginationOptions } from './PaginationOptions';
+export { PagedResponse } from './PagedResponse';
+export { Response } from './Response';
+export { Request } from './Request';
+export { Filter } from './Filter';
+export { Route } from './Route';
--- src/@types/index.ts
+++ src/@types/index.ts
@@ -1,9 +1,5 @@
-export { SearchOptions } from './SearchOptions';
-export { SortOrder } from './SortOrder';
-export { GetRequestOptions } from './GetRequestOptions';
-export { PaginationOptions } from './PaginationOptions';
-export { PagedResponse } from './PagedResponse';
-export { Response } from './Response';
-export { Request } from './Request';
-export { Filter } from './Filter';
-export { Route } from './Route';
+import * as Api from './api';
+
+export {
+  Api,
+};
