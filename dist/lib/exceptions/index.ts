98325a4915eddc25f4e789a6504c0ea6ba318b7d
Ronnie Nyaga <ronnienyaga@gmail.com>
2019-12-24 17:15:42 +0300

    Exposing the exceptions

---
+++ dist/lib/exceptions/index.ts
@@ -0,0 +1,11 @@
+import InternalException from './InternalException';
+import ResourceNotFoundException from './ResourceNotFoundException';
+import UnauthorizedException from './UnauthorizedException';
+import BadRequestException from './BadRequestException';
+
+export {
+  UnauthorizedException,
+  ResourceNotFoundException,
+  InternalException,
+  BadRequestException,
+};
